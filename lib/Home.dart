0cf7ce14ab74bd504711c2035bc54ae866ade12a
dvalerio <dnotms290>
2019-10-31 16:02:25 -0300

    Interface inicial

--- lib/Home.dart
+++ lib/Home.dart
@@ -6,8 +6,79 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  TextEditingController _controllerCampo = TextEditingController();
+
+  _salvar(){
+
+  }
+
+  _recuperar(){
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Manipulação de dados"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(30),
+              child: Text("Dado",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                keyboardType: TextInputType.text,
+                decoration:
+                InputDecoration(labelText: "Digite algo"),
+                style: TextStyle(fontSize: 20),
+                controller: _controllerCampo,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: Row(
+                children: <Widget>[
+                  RaisedButton(
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    padding: EdgeInsets.all(15),
+                    child: Text(
+                      "Salvar",
+                      style: TextStyle(
+                        fontSize: 20
+                      ),
+                    ),
+                    onPressed: _salvar,
+                  ),
+                  RaisedButton(
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    padding: EdgeInsets.all(15),
+                    child: Text(
+                      "Salvar",
+                      style: TextStyle(
+                          fontSize: 20
+                      ),
+                    ),
+                    onPressed: _recuperar,
+                  ),
+                ],
+              ),
+            )
+          ]
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,4 +4,5 @@ import 'Home.dart';
 
 void main() => runApp(MaterialApp(
   home: Home(),
+  debugShowCheckedModeBanner: false,
 ));
